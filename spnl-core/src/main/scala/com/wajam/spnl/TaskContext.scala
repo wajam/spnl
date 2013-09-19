09f6880955bffe5d84026d5201862bb0802f3e20
Olivier Toupin <olivier.toupin@wajam.com>
2013-09-19 11:56:41 -0400

    * TaskContext rates are now persisted

--- spnl-core/src/main/scala/com/wajam/spnl/TaskContext.scala
+++ spnl-core/src/main/scala/com/wajam/spnl/TaskContext.scala
@@ -12,17 +12,32 @@ case class TaskContext(var data:Map[String, Any] = Map[String, Any](),
                        var normalRate: Double = 1,
                        var throttleRate: Double = 1,
                        var maxConcurrent: Int = 5) {
+
+
+  private implicit val jsonFormats = Serialization.formats(NoTypeHints)
+
   def toJson: String = {
-    new String(jasonCodec.encode(JObject(List(JField("data", JsonCodec.toJValue(data)))), Encoding))
+
+    val items = List(
+      JField("data", JsonCodec.toJValue(data)),
+      JField("normalRate", JsonCodec.toJValue(normalRate)),
+      JField("throttleRate", JsonCodec.toJValue(throttleRate)),
+      JField("maxConcurrent", JsonCodec.toJValue(maxConcurrent)))
+
+    new String(jsonCodec.encode(JObject(items), Encoding))
   }
 
   def updateFromJson(json: String) {
-    val values = jasonCodec.decode(json.getBytes, Encoding).asInstanceOf[JObject].values
+    val values = jsonCodec.decode(json.getBytes, Encoding).asInstanceOf[JObject].values
+
     data = values("data").asInstanceOf[Map[String, Any]]
+    values.get("normalRate").map(v => normalRate = v.asInstanceOf[Double])
+    values.get("throttleRate").map(v => throttleRate = v.asInstanceOf[Double])
+    values.get("maxConcurrent").map(v => maxConcurrent = v.asInstanceOf[BigInt].toInt)
   }
 }
 
 object TaskContext {
   private val Encoding = "UTF-8"
-  private val jasonCodec = new JsonCodec()
+  private val jsonCodec = new JsonCodec()
 }
