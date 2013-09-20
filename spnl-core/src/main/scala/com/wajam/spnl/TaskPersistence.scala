3bb823b2dd1c322aed15dd8d92098eb39ed13c93
Olivier Toupin <olivier.toupin@wajam.com>
2013-09-20 11:11:13 -0400

    * Two Z is better

--- spnl-core/src/main/scala/com/wajam/spnl/TaskPersistence.scala
+++ spnl-core/src/main/scala/com/wajam/spnl/TaskPersistence.scala
@@ -3,7 +3,7 @@ package com.wajam.spnl
 import com.wajam.nrv.service.{Service, ServiceMember}
 
 /**
- * Persists tasks and their dataz
+ * Persists tasks and their datazz
  */
 trait TaskPersistence {
   def saveTask(task: Task)
